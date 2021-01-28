e3a26f3ea60583ac9ed4c1de636de165a780ac76
Mateo Ferreira <matteo948@icloud.com>
2021-01-28 17:01:13 -0500

    functions prompts

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const inquire = require("inquirer")
 const { describe } = require("yargs")
 
+inquire.prompt([
+
+    {
+        type: "input",
+        name: "mail",
+        message:"what is your email",
+        validate: function (input) 
+        {
+         if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input)
+          {
+            return (true)
+          }
+            alert("You have entered an invalid email address!")
+            return (false)
+        }
+    }
+]).then(function(response){
+    const data =  `${response.mail}`
+})
